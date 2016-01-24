d598c5d9dca4ca3690b9ea8f2e216cc5159024dd
Aaron O'Mullan <aaron.omullan@gmail.com>
2016-01-23 17:32:46 -0800

    Fail image test after error (prevent panic)

--- bing/image_test.go
+++ bing/image_test.go
@@ -9,6 +9,7 @@ func TestFetchHTML(t *testing.T) {
 	results, err := Search("birthday cake")
 	if err != nil {
 		t.Errorf("error searching: %s", err)
+		return
 	}
 	fmt.Printf("got back %s", results[0].URL)
 }
